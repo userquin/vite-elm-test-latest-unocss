4ab846b404470fc53bd603ab9c22b175b2be950e
userquin <userquin@gmail.com>
2023-05-31 20:43:26 +0200

    chore: add minimal UnoCSS

--- src/HelloWorld.elm
+++ src/HelloWorld.elm
@@ -1,7 +1,7 @@
 module HelloWorld exposing (helloWorld)
 
 import Html exposing (Html, a, button, code, div, h1, p, text)
-import Html.Attributes exposing (href)
+import Html.Attributes exposing (class, href)
 import Html.Events exposing (onClick)
 import Msg exposing (Msg(..))
 
@@ -9,7 +9,7 @@ import Msg exposing (Msg(..))
 helloWorld : Int -> Html Msg
 helloWorld model =
     div []
-        [ h1 [] [ text "Hello, Vite + Elm!" ]
+        [ h1 [class "mt-2em animate-bounce-alt animate-duration-2s"] [ text "Hello, Vite + Elm!" ]
         , p []
             [ a [ href "https://vitejs.dev/guide/features.html" ] [ text "Vite Documentation" ]
             , text " | "
@@ -20,7 +20,7 @@ helloWorld model =
         , button [ onClick Decrement ] [ text "-" ]
         , p []
             [ text "Edit "
-            , code [] [ text "src/Main.elm" ]
+            , code [] [ text "./src/Main.elm" ]
             , text " to test auto refresh"
             ]
         ]
